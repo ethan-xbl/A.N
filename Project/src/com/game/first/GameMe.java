8c7a5f06481d380d7608fdac52e592124ec6591b
Lin home <lin.shawben@gmail.com>
2011-06-12 01:47:22 +0800

    first commit

---
+++ Project/src/com/game/first/GameMe.java
@@ -0,0 +1,13 @@
+package com.game.first;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameMe extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
